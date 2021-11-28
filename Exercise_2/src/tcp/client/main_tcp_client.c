d4e53f3c062dd58433cc8da1b56a7394be7be770
scastd00 <scastd00@estudiantes.unileon.es>
2021-11-28 04:41:49 +0100

    Added TCP documentation

--- Exercise_2/src/tcp/client/main_tcp_client.c
+++ Exercise_2/src/tcp/client/main_tcp_client.c
@@ -11,6 +11,15 @@
 
 #define IP "127.0.0.1"
 
+/**
+ * Reads the response sent by the server.
+ *
+ * @param socket socket to read from.
+ * @param message string used to store the data read.
+ * @param n_bytes maximum number of bytes to read.
+ *
+ * @return the bytes read from the socket.
+ */
 ssize_t receiveResponse(int socket, char *message, int n_bytes) {
 	ssize_t n = read(socket, message, n_bytes);
 
@@ -20,84 +29,104 @@ ssize_t receiveResponse(int socket, char *message, int n_bytes) {
 	return n;
 }
 
+/**
+ * Create a new stream (TCP) socket.
+ *
+ * @return
+ */
 int createSocket() {
-	// Create client socket of the STREAM TYPE (TCP)
 	int sock = socket(AF_INET, SOCK_STREAM, 0);
 
-	// Create a local socket-address for the client
+	// Create the socket address for the client
 	struct sockaddr_in client;
 	client.sin_family = AF_INET;
 	client.sin_port = INADDR_ANY;
 	client.sin_addr.s_addr = INADDR_ANY;
 
 	bind(sock, (struct sockaddr *) &client, sizeof(client));
-
 	return sock;
 }
 
+/**
+ * Establish the connection with the server.
+ *
+ * @param sock socket used to connect with the server.
+ * @param ip_address IP address of the server.
+ * @param port port in which the server is listening.
+ *
+ * @return 0 if the connection was established successfully, 1 otherwise.
+ */
 int connectToServer(int sock, const char *ip_address, uint16_t port) {
-
-	//Socket address for server
-	struct sockaddr_in server;
-	server.sin_family = AF_INET;
-	server.sin_port = htons(port);
-	server.sin_addr.s_addr = inet_addr(ip_address);
+	struct sockaddr_in server = { // Socket address of the server
+		.sin_family = AF_INET,
+		.sin_port = htons(port),
+		.sin_addr.s_addr = inet_addr(ip_address)
+	};
 
 	/*
 	 * Assuming the server's welcome socket is in the LISTEN state, the
-	 * following call will cause the local TCP/IP stack to initiate the
-	 * 3-way. From this moment, the connection is in the ESTABLISHED state.
+	 * following call will cause the initialization of the 3-way handshake.
+	 * From this moment, the connection is in the ESTABLISHED state.
 	 */
-	int r = connect(sock, (struct sockaddr *) &server, sizeof(server));
-
-	fflush(stdout);
-
-	return r;
+	return connect(sock, (struct sockaddr *) &server, sizeof(server));
 }
 
+/**
+ * Prints the time in a human-readable format.
+ *
+ * @param seconds_to_add seconds since 01-01-1900.
+ * @param base_date date to add the seconds.
+ */
 void print_time(const long seconds_to_add, struct tm base_date) {
 	time_t raw_time = mktime(&base_date) + seconds_to_add;
 	char buff[80];
 
-	// Format time, "ddd yyyy-mm-dd hh:mm:ss zzz"
+	// Format time, "day, dd mm yyyy hh:mm:ss TZ"
 	struct tm ts = *localtime(&raw_time);
-	strftime(buff, sizeof(buff), "%a %Y-%m-%d %H:%M:%S %Z", &ts);
+	strftime(buff, sizeof(buff), "%a, %d %b %Y %H:%M:%S %Z", &ts);
 	printf("%s\n", buff);
+	fflush(stdout);
 }
 
+/**
+ * All the functionality of the program.
+ *
+ * @param sock socket connected to the server.
+ */
 void run_tcp(int sock) {
 	// An additional byte for storing the null character
-	char *received = malloc(MAX_RES + 1);
-	receiveResponse(sock, received, MAX_RES);
+	char *data_received = malloc(MAX_RES + 1);
+	receiveResponse(sock, data_received, MAX_RES);
 
-	struct tm tm_1900 = {
+	struct tm tm_1900 = { // Create the base date (01-01-1900)
+		.tm_year = 0, // Year 1900
+		.tm_mon = 0, // January
 		.tm_sec = 0,
 		.tm_min = 0,
 		.tm_hour = 0,
-		.tm_mday = 1,
-		.tm_mon = 0,
-		.tm_year = 0, // Year 1900
+		.tm_mday = 1 // Day 1
 	};
 
-	print_time(atol(received), tm_1900);
-
-	fflush(stdout);
-	free(received);
+	print_time(atol(data_received), tm_1900);
+	free(data_received);
 }
 
-void client(const char *ip, const uint16_t port) {
+/**
+ * Main function of the program.
+ *
+ * @return 0 if the program completes successfully
+ */
+int main() {
 	int sock = createSocket();
-	int r = connectToServer(sock, ip, port);
+	int r = connectToServer(sock, IP, DEFAULT_PORT);
 
 	if (r != 0) {
+		perror("Connection to server failed\n");
+		close(sock);
 		exit(r);
 	}
 
 	run_tcp(sock);
 	close(sock);
-}
-
-int main() {
-	client(IP, DEFAULT_PORT);
 	return 0;
 }
--- Exercise_2/src/tcp/server/Server.java
+++ Exercise_2/src/tcp/server/Server.java
@@ -8,42 +8,65 @@ import java.util.Calendar;
 import java.util.TimeZone;
 
 public class Server {
-	private static final int          PORT = 37;
-	private final        ServerSocket serverSocket;
-	private              Socket       s;
 
+	/**
+	 * Timeserver default port <a href="https://datatracker.ietf.org/doc/html/rfc868">RFC868</a>.
+	 */
+	private static final int PORT = 37;
+
+	/**
+	 * Socket for the server.
+	 */
+	private final ServerSocket serverSocket;
+
+	/**
+	 * Class constructor that builds the {@link ServerSocket}.
+	 *
+	 * @throws IOException if an error occurs.
+	 */
 	public Server() throws IOException {
 		this.serverSocket = new ServerSocket(PORT);
 	}
 
+	/**
+	 * All the functionality of the server.
+	 *
+	 * @throws IOException if an error occurs.
+	 */
 	public void work() throws IOException {
-		s = this.serverSocket.accept();
+		// Listen state and create a delegate socket on connection
+		Socket s = this.serverSocket.accept();
 
 		OutputStream out = s.getOutputStream();
 
 		byte[] data = Long.toString(this.getTime()).getBytes();
-		out.write(data);
+		out.write(data); // Send the date
 
-		System.out.printf("Sent %d bytes%n", data.length);
-	}
+		System.out.printf("Sent %d bytes\n", data.length);
 
-	public void closeConnection() throws IOException {
+		// Close the sockets
 		s.close();
 		this.serverSocket.close();
 	}
 
+	/**
+	 * Calculate the seconds since 01-01-1900 until now.
+	 *
+	 * @return the number of seconds since 01-01-1900.
+	 */
 	private long getTime() {
 		Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("GMT"));
 		cal.clear();
 		cal.set(1900, Calendar.JANUARY, 1, 0, 0, 0);
 
+		/*
+		 * 2 calculations
+		 *   msFrom1900ToEpoch ==> 01-01-1900 -> 01-01-1970
+		 *   msFrom1970ToToday ==> 01-01-1970 -> Today
+		 */
 		long msFrom1900ToEpoch = cal.getTimeInMillis() / 1000;
-		long msFrom1970ToToday = Calendar.getInstance().getTimeInMillis() / 1000;
+		long msFrom1970ToToday = Calendar.getInstance(TimeZone.getTimeZone("GMT")).getTimeInMillis() / 1000;
 
-		/* 2 calculations
-			- 01-01-1900 -> 01-01-1970
-			- 01-01-1970 -> Today
-		*/
-		return -msFrom1900ToEpoch + msFrom1970ToToday;
+		return msFrom1970ToToday - msFrom1900ToEpoch;
 	}
 }
